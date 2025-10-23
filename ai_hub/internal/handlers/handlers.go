2a1fba292b5a1814aff503aa9c354a47e6ac42e6
Ivan Lylin <ivanlylin@MacBook-Air-Ivan.local>
2025-10-23 21:26:33 +0300

    add dto

--- ai_hub/internal/handlers/handlers.go
+++ ai_hub/internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"lite_ai_hub/ai_hub/internal/dto"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/log"
 )
 
 type Handlers struct{}
@@ -12,10 +13,11 @@ func NewHandlers() *Handlers {
 	return &Handlers{}
 }
 
-func (h *Handlers) HandleMessage(c *fiber.Ctx) {
+func (h *Handlers) HandleMessage(c fiber.Ctx) {
 	var session dto.Session
-	if err := c.BodyParser(&session); err != nil {
-		return err
+	if err := c.Bind().Body(&session); err != nil {
+		log.Error(err)
+		return
 	}
 	// do smth with session
 }
