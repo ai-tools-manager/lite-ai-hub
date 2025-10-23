017fa935d49d005b43cad0906ce64edbd3ba7330
Ivan Lylin <ivanlylin@MacBook-Air-Ivan.local>
2025-10-23 19:12:28 +0300

    add handler

--- ai_hub/internal/handlers/handlers.go
+++ ai_hub/internal/handlers/handlers.go
@@ -1,1 +1,21 @@
 package handlers
+
+import (
+	"lite_ai_hub/ai_hub/internal/dto"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type Handlers struct{}
+
+func NewHandlers() *Handlers {
+	return &Handlers{}
+}
+
+func (h *Handlers) HandleMessage(c *fiber.Ctx) {
+	var session dto.Session
+	if err := c.BodyParser(&session); err != nil {
+		return err
+	}
+	// do smth with session
+}
